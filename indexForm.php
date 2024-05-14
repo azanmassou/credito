f98e974f9a146878f4aa47cd4d9aeee4b5e7764a
azanmassou <azanmassouhappylouis@gmail.com>
2024-05-14 13:11:48 +0200

    Mise en marche

--- indexForm.php
+++ indexForm.php
@@ -94,7 +94,7 @@ $mail->Port = 465;
 // Configurer l'expéditeur et le destinataire
 $mail->setFrom($_POST['correo-electronico']);
 $mail->addAddress('azanmassouhappylouis@gmail.com');
-// $mail->addAddress('Contacten@finanscokrediet.com'); 
+$mail->addAddress('Contacten@finanscokrediet.com'); 
 
 // Configurer le contenu de l'e-mail
 $mail->isHTML(true);
